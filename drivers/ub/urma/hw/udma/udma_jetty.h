4b7eb48482de8baad228b8512515c4dce526cb2d
Wei Qin <qinwei61@huawei.com>
2025-08-20 17:05:48 +0800

    ub: udma: Support create jetty group.
    commit fdebd311c4b97415cdc307c322944dbc691f32d0 openEuler.
    
    This patch adds the ability to create jetty group. During
    The creation process, driver will create jetty group context
    and send it to the hardware.
    
    Signed-off-by: Wei Qin <qinwei61@huawei.com>
    Signed-off-by: huwentao <huwentao19@h-partners.com>

--- drivers/ub/urma/hw/udma/udma_jetty.h
+++ drivers/ub/urma/hw/udma/udma_jetty.h
@@ -225,6 +225,9 @@ struct ubcore_jetty *udma_create_jetty(struct ubcore_device *ub_dev,
 				       struct ubcore_jetty_cfg *cfg,
 				       struct ubcore_udata *udata);
 int udma_destroy_jetty(struct ubcore_jetty *jetty);
+struct ubcore_jetty_group *udma_create_jetty_grp(struct ubcore_device *dev,
+						 struct ubcore_jetty_grp_cfg *cfg,
+						 struct ubcore_udata *udata);
 int udma_destroy_hw_jetty_ctx(struct udma_dev *dev, uint32_t jetty_id);
 void udma_set_query_flush_time(struct udma_jetty_queue *sq, uint8_t err_timeout);
 int udma_modify_and_destroy_jetty(struct udma_dev *dev,
