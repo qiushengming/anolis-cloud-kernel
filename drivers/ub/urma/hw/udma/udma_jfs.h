35f368c59d6a5b6c683ee5ba66b16b38946e35c9
Wei Qin <qinwei61@huawei.com>
2025-08-20 10:16:51 +0800

    ub: udma: Support create jfs.
    commit 30b22bb34e1059055f57a01f01520afe1b104545 openEuler.
    
    This patch adds the ability to create jfs, During the
    creation process, driver will create jfs context and
    send it to the hardware.
    
    Signed-off-by: Wei Qin <qinwei61@huawei.com>
    Signed-off-by: huwentao <huwentao19@h-partners.com>

--- drivers/ub/urma/hw/udma/udma_jfs.h
+++ drivers/ub/urma/hw/udma/udma_jfs.h
@@ -6,6 +6,17 @@
 
 #include "udma_common.h"
 
+#define MAX_WQEBB_NUM 4
+#define UDMA_JFS_WQEBB_SIZE 64
+#define UDMA_JFS_SGE_SIZE 16
+
+#define SQE_WRITE_NOTIFY_CTL_LEN 80
+
+enum udma_jfs_type {
+	UDMA_NORMAL_JFS_TYPE,
+	UDMA_KERNEL_STARS_JFS_TYPE,
+};
+
 struct udma_jfs {
 	struct ubcore_jfs ubcore_jfs;
 	struct udma_jetty_queue sq;
@@ -26,5 +37,20 @@ static inline struct udma_jfs *to_udma_jfs_from_queue(struct udma_jetty_queue *q
 {
 	return container_of(queue, struct udma_jfs, sq);
 }
+
+static inline uint32_t sq_cal_wqebb_num(uint32_t sqe_ctl_len, uint32_t sge_num)
+{
+	return (sqe_ctl_len + (sge_num - 1) * UDMA_JFS_SGE_SIZE) /
+		UDMA_JFS_WQEBB_SIZE + 1;
+}
+
+struct ubcore_jfs *udma_create_jfs(struct ubcore_device *ub_dev,
+				   struct ubcore_jfs_cfg *cfg,
+				   struct ubcore_udata *udata);
+int udma_alloc_u_sq_buf(struct udma_dev *dev, struct udma_jetty_queue *sq,
+			struct udma_create_jetty_ucmd *ucmd);
+int udma_alloc_k_sq_buf(struct udma_dev *dev, struct udma_jetty_queue *sq,
+			struct ubcore_jfs_cfg *jfs_cfg);
+void udma_free_sq_buf(struct udma_dev *dev, struct udma_jetty_queue *sq);
 
 #endif /* __UDMA_JFS_H__ */
