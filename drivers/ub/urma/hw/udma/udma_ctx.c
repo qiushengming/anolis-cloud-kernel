88e25f2f499bc5a9becf79f54b9a802c6cec7a69
JiaWei Kang <kangjiawei2@huawei.com>
2025-09-09 20:37:38 +0800

    ub: udma: jetty and rct support the hugepage buffer.
    commit 551b2ed34422a838904274d72789a2a99e29434a openEuler.
    
    This patch adds the ability to
    jetty and rct can use the hugepage buffer.
    
    Signed-off-by: JiaWei Kang <kangjiawei2@huawei.com>
    Signed-off-by: huwentao <huwentao19@h-partners.com>

--- drivers/ub/urma/hw/udma/udma_ctx.c
+++ drivers/ub/urma/hw/udma/udma_ctx.c
@@ -31,6 +31,7 @@ static int udma_init_ctx_resp(struct udma_dev *dev, struct ubcore_udrv_priv *udr
 	resp.die_id = dev->die_id;
 	resp.dump_aux_info = dump_aux_info;
 	resp.jfr_sge = dev->caps.jfr_sge;
+	resp.hugepage_enable = ubase_adev_prealloc_supported(dev->comdev.adev);
 
 	byte = copy_to_user((void *)(uintptr_t)udrv_data->out_addr, &resp,
 			   (uint32_t)sizeof(resp));
@@ -70,6 +71,8 @@ struct ubcore_ucontext *udma_alloc_ucontext(struct ubcore_device *ub_dev,
 	ctx->dev = dev;
 	INIT_LIST_HEAD(&ctx->pgdir_list);
 	mutex_init(&ctx->pgdir_mutex);
+	INIT_LIST_HEAD(&ctx->hugepage_list);
+	mutex_init(&ctx->hugepage_lock);
 
 	ret = udma_init_ctx_resp(dev, udrv_data);
 	if (ret) {
@@ -91,8 +94,11 @@ err_free_ctx:
 int udma_free_ucontext(struct ubcore_ucontext *ucontext)
 {
 	struct udma_dev *udma_dev = to_udma_dev(ucontext->ub_dev);
+	struct udma_hugepage_priv *priv;
+	struct vm_area_struct *vma;
 	struct udma_context *ctx;
 	int ret;
+	int i;
 
 	ctx = to_udma_context(ucontext);
 
@@ -103,7 +109,101 @@ int udma_free_ucontext(struct ubcore_ucontext *ucontext)
 	mutex_destroy(&ctx->pgdir_mutex);
 	ummu_sva_unbind_device(ctx->sva);
 
+	mutex_lock(&ctx->hugepage_lock);
+	list_for_each_entry(priv, &ctx->hugepage_list, list) {
+		if (current->mm) {
+			mmap_write_lock(current->mm);
+			vma = find_vma(current->mm, (unsigned long)priv->va_base);
+			if (vma != NULL && vma->vm_start <= (unsigned long)priv->va_base &&
+			    vma->vm_end >= (unsigned long)(priv->va_base + priv->va_len))
+				zap_vma_ptes(vma, (unsigned long)priv->va_base, priv->va_len);
+			mmap_write_unlock(current->mm);
+		}
+
+		dev_info(udma_dev->dev, "unmap_hugepage, 2m_page_num=%u.\n", priv->page_num);
+		for (i = 0; i < priv->page_num; i++)
+			__free_pages(priv->pages[i], get_order(UDMA_HUGEPAGE_SIZE));
+		kfree(priv->pages);
+		kfree(priv);
+	}
+	mutex_unlock(&ctx->hugepage_lock);
+	mutex_destroy(&ctx->hugepage_lock);
+
 	kfree(ctx);
+
+	return 0;
+}
+
+static int udma_mmap_jetty_dsqe(struct udma_dev *dev, struct ubcore_ucontext *uctx,
+				struct vm_area_struct *vma)
+{
+	struct ubcore_ucontext *jetty_uctx;
+	struct udma_jetty_queue *sq;
+	uint64_t address;
+	uint64_t j_id;
+
+	j_id = get_mmap_idx(vma);
+
+	xa_lock(&dev->jetty_table.xa);
+	sq = xa_load(&dev->jetty_table.xa, j_id);
+	if (!sq) {
+		dev_err(dev->dev,
+			"mmap failed, j_id: %llu not exist\n", j_id);
+		xa_unlock(&dev->jetty_table.xa);
+		return -EINVAL;
+	}
+
+	if (sq->is_jetty)
+		jetty_uctx = to_udma_jetty_from_queue(sq)->ubcore_jetty.uctx;
+	else
+		jetty_uctx = to_udma_jfs_from_queue(sq)->ubcore_jfs.uctx;
+
+	if (jetty_uctx != uctx) {
+		dev_err(dev->dev,
+			"mmap failed, j_id: %llu, uctx invalid\n", j_id);
+		xa_unlock(&dev->jetty_table.xa);
+		return -EINVAL;
+	}
+	xa_unlock(&dev->jetty_table.xa);
+
+	address = (uint64_t)dev->db_base + JETTY_DSQE_OFFSET + j_id * UDMA_HW_PAGE_SIZE;
+
+	if (io_remap_pfn_range(vma, vma->vm_start, address >> PAGE_SHIFT,
+			       PAGE_SIZE, vma->vm_page_prot))
+		return -EAGAIN;
+
+	return 0;
+}
+
+static int udma_mmap_hugepage(struct udma_dev *dev, struct ubcore_ucontext *uctx,
+			      struct vm_area_struct *vma)
+{
+	uint32_t max_map_size = dev->caps.cqe_size * dev->caps.jfc.depth;
+	uint32_t map_size = vma->vm_end - vma->vm_start;
+
+	if (!IS_ALIGNED(map_size, UDMA_HUGEPAGE_SIZE)) {
+		dev_err(dev->dev, "mmap size is not 2m alignment.\n");
+		return -EINVAL;
+	}
+
+	if (map_size == 0) {
+		dev_err(dev->dev, "mmap size is zero.\n");
+		return -EINVAL;
+	}
+
+	if (map_size > max_map_size) {
+		dev_err(dev->dev, "mmap size(%u) is greater than the max_size.\n",
+			map_size);
+		return -EINVAL;
+	}
+
+	vm_flags_set(vma, VM_IO | VM_LOCKED | VM_DONTEXPAND | VM_DONTDUMP | VM_DONTCOPY);
+	vma->vm_page_prot = __pgprot(((~PTE_ATTRINDX_MASK) & vma->vm_page_prot.pgprot) |
+				     PTE_ATTRINDX(MT_NORMAL));
+	if (udma_alloc_u_hugepage(to_udma_context(uctx), vma)) {
+		dev_err(dev->dev, "failed to alloc hugepage.\n");
+		return -ENOMEM;
+	}
 
 	return 0;
 }
@@ -112,11 +212,6 @@ int udma_mmap(struct ubcore_ucontext *uctx, struct vm_area_struct *vma)
 {
 #define JFC_DB_UNMAP_BOUND 1
 	struct udma_dev *udma_dev = to_udma_dev(uctx->ub_dev);
-	struct ubcore_ucontext *jetty_uctx;
-	struct udma_jetty_queue *sq;
-	resource_size_t db_addr;
-	uint64_t address;
-	uint64_t j_id;
 	uint32_t cmd;
 
 	if (((vma->vm_end - vma->vm_start) % PAGE_SIZE) != 0) {
@@ -125,7 +220,6 @@ int udma_mmap(struct ubcore_ucontext *uctx, struct vm_area_struct *vma)
 		return -EINVAL;
 	}
 
-	db_addr = udma_dev->db_base;
 	vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
 
 	cmd = get_mmap_cmd(vma);
@@ -133,41 +227,15 @@ int udma_mmap(struct ubcore_ucontext *uctx, struct vm_area_struct *vma)
 	case UDMA_MMAP_JFC_PAGE:
 		if (io_remap_pfn_range(vma, vma->vm_start,
 				       jfc_arm_mode > JFC_DB_UNMAP_BOUND ?
-				       (uint64_t)db_addr >> PAGE_SHIFT :
+				       (uint64_t)udma_dev->db_base >> PAGE_SHIFT :
 				       page_to_pfn(udma_dev->db_page),
 				       PAGE_SIZE, vma->vm_page_prot))
 			return -EAGAIN;
 		break;
 	case UDMA_MMAP_JETTY_DSQE:
-		j_id = get_mmap_idx(vma);
-		xa_lock(&udma_dev->jetty_table.xa);
-		sq = xa_load(&udma_dev->jetty_table.xa, j_id);
-		if (!sq) {
-			dev_err(udma_dev->dev,
-				"mmap failed, j_id: %llu not exist\n", j_id);
-			xa_unlock(&udma_dev->jetty_table.xa);
-			return -EINVAL;
-		}
-
-		if (sq->is_jetty)
-			jetty_uctx = to_udma_jetty_from_queue(sq)->ubcore_jetty.uctx;
-		else
-			jetty_uctx = to_udma_jfs_from_queue(sq)->ubcore_jfs.uctx;
-
-		if (jetty_uctx != uctx) {
-			dev_err(udma_dev->dev,
-				"mmap failed, j_id: %llu, uctx invalid\n", j_id);
-			xa_unlock(&udma_dev->jetty_table.xa);
-			return -EINVAL;
-		}
-		xa_unlock(&udma_dev->jetty_table.xa);
-
-		address = (uint64_t)db_addr + JETTY_DSQE_OFFSET + j_id * UDMA_HW_PAGE_SIZE;
-
-		if (io_remap_pfn_range(vma, vma->vm_start, address >> PAGE_SHIFT,
-				       PAGE_SIZE, vma->vm_page_prot))
-			return -EAGAIN;
-		break;
+		return udma_mmap_jetty_dsqe(udma_dev, uctx, vma);
+	case UDMA_MMAP_HUGEPAGE:
+		return udma_mmap_hugepage(udma_dev, uctx, vma);
 	default:
 		dev_err(udma_dev->dev,
 			"mmap failed, cmd(%u) not support\n", cmd);
