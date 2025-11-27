8dbd9f56d453e07ff555d148dede30c70e86f35b
Wen Chen <chenwen54@huawei.com>
2025-11-27 13:21:45 +0800

    urma: added functions for getting and setting TP attribute fields.
    ANBZ: #IJSBBS
    
    commit 48c0dc7e6efc81a43d09cc96efcd43f68c0ed145 openEuler
    
    This patch primarily adds two interface functions: ubcore_set_tp_attr
    and ubcore_get_tp_attr.
    
    The implementation provides the following key functionalities:
    
    1. The `ubcore_set_tp_attr` function can specify attribute fields
    of already created TPs.
    
    2. The `ubcore_set_tp_attr` function retrieves attribute fields
    of already created TPs.
    
    Signed-off-by: Wen Chen <chenwen54@huawei.com>
    Signed-off-by: qiushengming <qiushengming1@huawei.com>

--- drivers/ub/urma/ubcore/ubcore_tp.c
+++ drivers/ub/urma/ubcore/ubcore_tp.c
@@ -39,3 +39,48 @@ int ubcore_get_tp_list(struct ubcore_device *dev, struct ubcore_get_tp_cfg *cfg,
 }
 EXPORT_SYMBOL(ubcore_get_tp_list);
 
+int ubcore_set_tp_attr(struct ubcore_device *dev, const uint64_t tp_handle,
+		       const uint8_t tp_attr_cnt, const uint32_t tp_attr_bitmap,
+		       const struct ubcore_tp_attr_value *tp_attr,
+		       struct ubcore_udata *udata)
+{
+	int ret;
+
+	if (dev == NULL || dev->ops == NULL || dev->ops->set_tp_attr == NULL ||
+	    tp_attr == NULL) {
+		ubcore_log_err("Invalid parameter.\n");
+		return -EINVAL;
+	}
+
+	ret = dev->ops->set_tp_attr(dev, tp_handle, tp_attr_cnt, tp_attr_bitmap,
+				    tp_attr, udata);
+	if (ret != 0)
+		ubcore_log_err("Failed to set tp attributions, ret: %d.\n",
+			       ret);
+
+	return ret;
+}
+EXPORT_SYMBOL(ubcore_set_tp_attr);
+
+int ubcore_get_tp_attr(struct ubcore_device *dev, const uint64_t tp_handle,
+		       uint8_t *tp_attr_cnt, uint32_t *tp_attr_bitmap,
+		       struct ubcore_tp_attr_value *tp_attr,
+		       struct ubcore_udata *udata)
+{
+	int ret;
+
+	if (dev == NULL || dev->ops == NULL || dev->ops->get_tp_attr == NULL ||
+	    tp_attr_cnt == NULL || tp_attr_bitmap == NULL || tp_attr == NULL) {
+		ubcore_log_err("Invalid parameter.\n");
+		return -EINVAL;
+	}
+
+	ret = dev->ops->get_tp_attr(dev, tp_handle, tp_attr_cnt, tp_attr_bitmap,
+				    tp_attr, udata);
+	if (ret != 0)
+		ubcore_log_err("Failed to get tp attributions, ret: %d.\n",
+			       ret);
+
+	return ret;
+}
+EXPORT_SYMBOL(ubcore_get_tp_attr);
