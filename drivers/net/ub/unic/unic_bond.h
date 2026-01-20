e775782142aaf602aed1396f3d0457b2fe67c930
Xiongchuan Zhou <zhouxiongchuan@h-partners.com>
2026-01-20 19:11:52 +0800

    net: unic: Notify mami of the bond ip change.
    commit 38e337c119e72727d3d56c8ccb928567ad5b249c openEuler.
    
    In the UBOE mode, if the IP address of the bond interface device in the
    protocol stack changes, the bond IP event is added as a linked list node.
    A scheduled task is then added to traverse and send the bond IP changes.
    The changed IP address, subnet mask, and actions are sent to the MAMI
    through the ctrlq message channel.
    
    Signed-off-by: Xiongchuan Zhou <zhouxiongchuan@h-partners.com>
    Signed-off-by: huwentao <huwentao19@h-partners.com>

---
+++ drivers/net/ub/unic/unic_bond.h
@@ -0,0 +1,44 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2026 HiSilicon Technologies Co., Ltd. All rights reserved.
+ *
+ */
+
+#ifndef __UNIC_BOND_H__
+#define __UNIC_BOND_H__
+
+#include "unic_comm_addr.h"
+
+#define UNIC_BOND_IP_REQ_SIZE		4
+#define UNIC_PATTERN3_MODE		1
+
+enum unic_bond_ip_notify_cmd {
+	UNIC_BOND_IP_NOTIFY_CMD_ADD = 0x00,
+	UNIC_BOND_IP_NOTIFY_CMD_DEL = 0x01,
+};
+
+struct unic_bond_ip_notify_req {
+	__le16	ip_cmd;
+	__le16	ip_mask;
+	__le32	ip_addr[UNIC_BOND_IP_REQ_SIZE];
+};
+
+enum unic_bond_port_change_cmd {
+	UNIC_CTRLQ_BOND_DEL_PORT = 0x00,
+	UNIC_CTRLQ_BOND_ADD_PORT = 0x01,
+};
+
+static inline bool unic_bond_ip_sync_supported(struct unic_dev *unic_dev)
+{
+	struct auxiliary_device *adev = unic_dev->comdev.adev;
+	struct ubase_caps *caps = ubase_get_dev_caps(adev);
+
+	return caps->packet_pattern_mode == UNIC_PATTERN3_MODE;
+}
+
+void unic_sync_bond_ip_table(struct unic_dev *unic_dev);
+void unic_uninit_bond_ip_table(struct unic_dev *unic_dev);
+int unic_update_bond_ipaddr(struct unic_dev *unic_dev, struct sockaddr *sa,
+			    u16 ip_mask, enum UNIC_COMM_ADDR_STATE state);
+
+#endif
