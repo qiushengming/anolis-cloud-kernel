cecc2f175429fbf1655e19d4fb38b021ddedcadf
Zhipeng Lu <luzhipeng8@h-partners.com>
2026-01-23 10:27:50 +0800

    ub: ubmempfd: supports for D2H mapping and demapping
    ANBZ: #IJSBBS
    
    commit 086a741d7b7143ca2b3a073a226fb781bb16a49d openEuler.
    
    This patch includes the support for D2H mapping and
    demapping functions, as well as the maintainability and
    testability.
    
    Signed-off-by: Zhipeng Lu <luzhipeng8@h-partners.com>
    Signed-off-by: qiushengming <qiushengming1@huawei.com>

--- include/uapi/ub/ubmempfd/ubmempfd.h
+++ include/uapi/ub/ubmempfd/ubmempfd.h
@@ -7,4 +7,23 @@
 #include <linux/kernel.h>
 #include <linux/types.h>
 
+enum ubmempfd_opcode {
+	UBMEMPFD_OPCODE_MAP = 0,
+	UBMEMPFD_OPCODE_UNMAP = 1,
+	UBMEMPFD_OPCODE_DEFAULT = 2
+};
+
+struct ubm_request {
+	__u32 opcode;
+	__u32 tid;
+	__u64 uba;
+	__u64 size;
+	__u64 areas_num;
+	struct {
+		__u64 hva;
+		__u64 size;
+	} areas[];
+};
+
 #endif /*_UAPI_UB_UBMEMPFD_UBMEMPFD_H_*/
+
