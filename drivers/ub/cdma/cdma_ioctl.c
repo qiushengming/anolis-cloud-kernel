b087803132456931809234dcb46da408c3e2aea9
Zhipeng Lu <luzhipeng8@h-partners.com>
2026-01-26 13:53:48 +0800

    ub: cdma: cdma uaf bugfix
    ANBZ: #IJSBBS
    
    commit ae7797e10b1793dd9b84e48a63913eb6e9eefe53 openEuler.
    
    cdma uaf bugfix
    
    Fixes: 35203448b9d1 ("ub: cdma: support reporting asynchronous events")
    
    Signed-off-by: Zhipeng Lu <luzhipeng8@h-partners.com>
    Signed-off-by: qiushengming <qiushengming1@huawei.com>

--- drivers/ub/cdma/cdma_ioctl.c
+++ drivers/ub/cdma/cdma_ioctl.c
@@ -127,7 +127,6 @@ static int cdma_delete_ucontext(struct cdma_ioctl_hdr *hdr,
 				struct cdma_file *cfile)
 {
 	struct cdma_dev *cdev = cfile->cdev;
-	struct cdma_jfae *jfae;
 
 	if (!cfile->uctx) {
 		dev_err(cdev->dev, "cdma context has not been created.\n");
@@ -140,10 +139,6 @@ static int cdma_delete_ucontext(struct cdma_ioctl_hdr *hdr,
 			cfile->uctx->handle);
 		return -EBUSY;
 	}
-
-	jfae = cfile->uctx->jfae;
-	if (jfae)
-		jfae->ctx = NULL;
 
 	cdma_free_context(cdev, cfile->uctx);
 	cfile->uctx = NULL;
