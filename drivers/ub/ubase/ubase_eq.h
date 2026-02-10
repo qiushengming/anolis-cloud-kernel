e8aa7b62ac59a35748c9263e04d2b31c1f068e87
Haiqing Fang <fanghaiqing@huawei.com>
2026-02-10 21:46:55 +0800

    ub: ubase: Traverse mailbox AE events using interrupts and polling.
    commit bc9c6116fed287b242042f4615ad3040ce857b91 openEuler.
    
    The driver issues a mailbox task to the chip for execution.
    After the chip completes the task, it generates an AE event
    and reports an interrupt. However, when multiple URMA tasks
    are executed concurrently, the time taken for the AE event
    interrupt generated by the mailbox task increases as the
    number of concurrent tasks increases. To minimize the time
    taken by the chip and OS for interrupt processing, the ubase
    driver uses a combination of interrupts and polling to
    handle mailbox tasks. During the initialization process, the
    ubase driver creates an AE thread. When the driver issues a
    mailbox task, it wakes up this thread to poll for AE events.
    
    To avoid concurrency with the interrupt process, a raw spin
    lock is added to the AE event polling process. The polling
    thread polls for AE events once, then waits for 1
    microsecond before polling again, to prevent the polling
    thread from holding the lock continuously, which would
    otherwise prevent the interrupt thread from obtaining the
    lock and getting blocked.
    
    To minimize CPU overhead, the AE thread polls for AE
    events for a maximum of 10 ms in each environment. After
    that, the thread sleeps for 30 ms and then polls for AE
    events again. The polling process will exit once the AE
    event corresponding to the mailbox is detected or the
    interrupt process has handled the AE event corresponding
    to the mailbox. If the AE event corresponding to the
    mailbox cannot be obtained, the polling process will
    also exit once the polling time exceeds the mailbox
    timeout.
    
    Fixes: 8d68017f37fa ("ub: ubase: support for command process")
    Signed-off-by: Haiqing Fang <fanghaiqing@huawei.com>
    Signed-off-by: huwentao <huwentao19@h-partners.com>

--- drivers/ub/ubase/ubase_eq.h
+++ drivers/ub/ubase/ubase_eq.h
@@ -7,6 +7,8 @@
 #ifndef __UBASE_EQ_H__
 #define __UBASE_EQ_H__
 
+#include <linux/sched.h>
+#include <linux/spinlock_types_raw.h>
 #include <ub/ubase/ubase_comm_eq.h>
 
 #include "ubase.h"
@@ -171,6 +173,9 @@ struct ubase_aeq {
 	struct ubase_dev	*udev;
 	struct ubase_eq		eq;
 	struct ubase_event_nb	cb[UBASE_AE_LEVEL_NUM];
+	struct completion	poll;
+	struct task_struct	*ae_task;
+	raw_spinlock_t		aeq_lock;
 };
 
 struct ubase_ceqs {
