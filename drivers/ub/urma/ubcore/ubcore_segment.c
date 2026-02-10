18f49bef3f4086ec569a7071750c88e28b78dfde
chenjingwei0113 <chenjingwei13@huawei.com>
2026-02-10 10:48:46 +0800

    urma: optimize log levels for kernel space
    ANBZ: #IJSBBS
    
    commit 51414489fee8e5bfbd0d933bbcf258e300efffd1 openEuler
    
    optimize log levels for kernel space
    
    Fixes: 37d3b62e2b71 ("urma: added support for ipoverurma")
    Signed-off-by: Chen JingWei <chenjingwei13@huawei.com>
    Signed-off-by: qiushengming <qiushengming1@huawei.com>

--- drivers/ub/urma/ubcore/ubcore_segment.c
+++ drivers/ub/urma/ubcore/ubcore_segment.c
@@ -30,19 +30,21 @@ struct ubcore_token_id *ubcore_alloc_token_id(struct ubcore_device *dev,
 	if (dev == NULL || dev->ops == NULL ||
 	    dev->ops->alloc_token_id == NULL ||
 	    dev->ops->free_token_id == NULL) {
-		ubcore_log_err("invalid parameter.\n");
 		return ERR_PTR(-EINVAL);
 	}
 
 	token_id = dev->ops->alloc_token_id(dev, flag, udata);
 	if (IS_ERR_OR_NULL(token_id)) {
-		ubcore_log_err("failed to alloc token_id id.\n");
+		ubcore_log_err("[DRV]:failed to alloc token_id id,dev_name is %s.\n",
+			dev->dev_name);
 		return UBCORE_CHECK_RETURN_ERR_PTR(token_id, ENOEXEC);
 	}
 	token_id->flag = flag;
 	token_id->ub_dev = dev;
 	token_id->uctx = ubcore_get_uctx(udata);
 	atomic_set(&token_id->use_cnt, 0);
+	ubcore_log_info("[ALLOC_TOKEN_ID]:device_name is %s, token_id is %u",
+		dev->dev_name, token_id->token_id);
 	return token_id;
 }
 EXPORT_SYMBOL(ubcore_alloc_token_id);
@@ -50,6 +52,9 @@ EXPORT_SYMBOL(ubcore_alloc_token_id);
 int ubcore_free_token_id(struct ubcore_token_id *token_id)
 {
 	struct ubcore_device *dev;
+	int use_cnt = atomic_read(&token_id->use_cnt);
+	uint32_t id = token_id->token_id;
+	int ret = 0;
 
 	if (token_id == NULL || token_id->ub_dev == NULL ||
 	    token_id->ub_dev->ops == NULL ||
@@ -60,10 +65,16 @@ int ubcore_free_token_id(struct ubcore_token_id *token_id)
 	dev = token_id->ub_dev;
 
 	if (atomic_read(&token_id->use_cnt)) {
-		ubcore_log_err("The token_id is still being used");
+		ubcore_log_err("The token_id is still being used, use_cnt is %d", use_cnt);
 		return -EBUSY;
 	}
-	return dev->ops->free_token_id(token_id);
+	ret = dev->ops->free_token_id(token_id);
+	if (ret != 0) {
+		ubcore_log_err("[DRV]Failed to free_token_id, ret is %d", ret);
+		return ret;
+	}
+	ubcore_log_info("[FREE_TOKEN_ID] Free_token_id is %u.", id);
+	return ret;
 }
 EXPORT_SYMBOL(ubcore_free_token_id);
 
@@ -133,14 +144,14 @@ struct ubcore_target_seg *ubcore_register_seg(struct ubcore_device *dev,
 		flag.bs.pa = cfg->flag.bs.pa;
 		tmp_cfg.token_id = ubcore_alloc_token_id(dev, flag, NULL);
 		if (IS_ERR_OR_NULL(tmp_cfg.token_id)) {
-			ubcore_log_err("alloc token id failed.\n");
 			return (void *)tmp_cfg.token_id;
 		}
 	}
 
 	tseg = dev->ops->register_seg(dev, &tmp_cfg, udata);
 	if (IS_ERR_OR_NULL(tseg)) {
-		ubcore_log_err_rl("UBEP failed to register segment.\n");
+		ubcore_log_err("[DRV]failed to register segment,dev name is %s, token_id is %u.\n",
+			dev->dev_name, tmp_cfg.token_id->token_id);
 		if (alloc_token_id == true)
 			(void)ubcore_free_token_id(tmp_cfg.token_id);
 		return UBCORE_CHECK_RETURN_ERR_PTR(tseg, ENOEXEC);
@@ -161,6 +172,8 @@ struct ubcore_target_seg *ubcore_register_seg(struct ubcore_device *dev,
 	atomic_set(&tseg->use_cnt, 0);
 	if (tseg->token_id != NULL)
 		atomic_inc(&tseg->token_id->use_cnt);
+	ubcore_log_info("[REGISTER SEG]Register seg,dev_name is %s, token_id is %u.",
+		dev->dev_name, tseg->token_id->token_id);
 
 	return tseg;
 }
@@ -192,10 +205,17 @@ int ubcore_unregister_seg(struct ubcore_target_seg *tseg)
 	}
 
 	ret = dev->ops->unregister_seg(tseg);
+	if (ret != 0) {
+		ubcore_log_err("[DRV]failed to unregister segment,dev name is %s, token_id is %u, ret is %d.\n",
+			dev->dev_name, token_id->token_id, ret);
+		return ret;
+	}
 
 	if (free_token_id == true && token_id != NULL)
 		(void)ubcore_free_token_id(token_id);
 
+	ubcore_log_info("[REGISTER SEG]Register seg,dev_name is %s, token_id is %u.",
+		dev->dev_name, token_id->token_id);
 	return ret;
 }
 EXPORT_SYMBOL(ubcore_unregister_seg);
@@ -216,28 +236,31 @@ struct ubcore_target_seg *ubcore_import_seg(struct ubcore_device *dev,
 		if (ubcore_connect_exchange_udata_when_import_seg(&cfg->seg,
 								  udata, dev) != 0) {
 			ubcore_log_err(
-				"failed to exchange udata when import seg\n");
+				"failed to exchange udata when import seg, dev_name is %s\n",
+				dev->dev_name);
 			return ERR_PTR(-ENOEXEC);
 		}
 	}
 
 	tseg = dev->ops->import_seg(dev, cfg, udata);
 	if (IS_ERR_OR_NULL(tseg)) {
-		ubcore_log_err("UBEP failed to import segment with va\n");
+		ubcore_log_err("[DRV] failed to import segment with va\n");
 		return UBCORE_CHECK_RETURN_ERR_PTR(tseg, ENOEXEC);
 	}
 	tseg->ub_dev = dev;
 	tseg->uctx = ubcore_get_uctx(udata);
 	tseg->seg = cfg->seg;
 	atomic_set(&tseg->use_cnt, 0);
-
+	ubcore_log_info("[IMPORT SEG] Import seg, dev_name is %s", dev->dev_name);
 	return tseg;
 }
 EXPORT_SYMBOL(ubcore_import_seg);
 
 int ubcore_unimport_seg(struct ubcore_target_seg *tseg)
 {
+	int ret = 0;
 	struct ubcore_device *dev;
+	uint32_t token_id = tseg->token_id->token_id;
 
 	if (tseg == NULL || tseg->ub_dev == NULL || tseg->ub_dev->ops == NULL ||
 	    tseg->ub_dev->ops->unimport_seg == NULL) {
@@ -245,7 +268,14 @@ int ubcore_unimport_seg(struct ubcore_target_seg *tseg)
 		return -EINVAL;
 	}
 	dev = tseg->ub_dev;
-
-	return dev->ops->unimport_seg(tseg);
+	ret = dev->ops->unimport_seg(tseg);
+	if (ret != 0) {
+		ubcore_log_err("[DRV] Failed to unimport seg, token_id is %u, dev_name is %s, ret is %d.",
+			token_id, dev->dev_name, ret);
+		return ret;
+	}
+	ubcore_log_info("[UNIMPORT SEG] Unimport seg, token_id is %u, dev_name is %s.",
+		token_id, dev->dev_name);
+	return ret;
 }
 EXPORT_SYMBOL(ubcore_unimport_seg);
