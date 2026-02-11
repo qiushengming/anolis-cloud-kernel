22ed936b6591090b90eca68ce26f6e61abd880ad
Haiqing Fang <fanghaiqing@huawei.com>
2026-02-11 16:19:42 +0800

    ub: ubase: Retain the initial tm qset configuration and restore it
    commit 6006f19fbfb5b1595e02865711dc4125cea04fc6 openEuler.
    
    When initializing a ub entity, ubase driver queries the configuration of
    the tm qset layer register as the default value. When the ubase driver
    is uninstalled, the configuration of the tm qset layer register is
    restored based on the default value.
    In addition, the ubase driver provides an api for restoring the
    configuration of the tm qset layer register. If a driver (for example,
    the unic driver) has a tool (for example, dcbtool) for configuring QoS
    and a requirement for restoring the configuration, the driver should
    call this api to restore the configuration when the driver is uninstalled.
    
    Fixes: acb96f3cf8c8 ("ub: ubase: support querying sl information in initialization phase")
    Signed-off-by: Haiqing Fang <fanghaiqing@huawei.com>
    Signed-off-by: huwentao <huwentao19@h-partners.com>

--- drivers/ub/ubase/ubase_hw.h
+++ drivers/ub/ubase/ubase_hw.h
@@ -186,5 +186,6 @@ int ubase_query_fst_fvt_rqmt(struct ubase_dev *udev,
 int ubase_query_port_bitmap(struct ubase_dev *udev);
 int __ubase_perf_stats(struct ubase_dev *udev, u64 port_bitmap, u32 period,
 		       struct ubase_perf_stats_result *data, u32 data_size);
+void ubase_qos_uninit(struct ubase_dev *udev);
 
 #endif
