bb10608f51faebbfe9ddf37e8d9917b904e88ed7
Xuanyu Pu <puxuanyu@huawei.com>
2026-02-11 17:24:24 +0800

    ub: ubase: Support for RC saved by queue granularity
    commit 1397f71d5ac5f7df7c4e3595455b0dacfa725d78 openEuler.
    
    This patch supports to resolve follow:
    
    1.The RC is saved by queue granularity.
    2.if the 2 MB huge page process is supported, the base address of each
    queue is calculated.
    3.If the 2 MB huge page process is not supported, use dma_alloc_coherent
    to allocate memory for each queue.
    
    Fixes: da7d25293a38 ("ub: ubase: support pre-alloc 2M pages.")
    Signed-off-by: Xuanyu Pu <puxuanyu@huawei.com>
    Signed-off-by: huwentao <huwentao19@h-partners.com>

--- drivers/ub/ubase/ubase_hw.h
+++ drivers/ub/ubase/ubase_hw.h
@@ -187,5 +187,7 @@ int ubase_query_port_bitmap(struct ubase_dev *udev);
 int __ubase_perf_stats(struct ubase_dev *udev, u64 port_bitmap, u32 period,
 		       struct ubase_perf_stats_result *data, u32 data_size);
 void ubase_qos_uninit(struct ubase_dev *udev);
+int ubase_rc_buf_init(struct ubase_dev *udev);
+void ubase_rc_buf_uninit(struct ubase_dev *udev);
 
 #endif
