21ba6a5bb9bf034a35570fb4657e6ea30f3f4a70
Jiaqi Cheng <chengjiaqi3@huawei.com>
2026-02-27 13:45:32 +0800

    ub:ub_fwctl:Support querying firmware version numbers
    commit 4ec624f1536463957e4cef48e723f4255d732637 openEuler.
    
    ubctl support querying firmware version numbers
    
    Signed-off-by: Jiaqi Cheng <chengjiaqi3@huawei.com>
    Signed-off-by: huwentao <huwentao19@h-partners.com>

--- include/uapi/fwctl/ub_fwctl.h
+++ include/uapi/fwctl/ub_fwctl.h
@@ -270,6 +270,10 @@ enum ub_fwctl_cmdrpc_type {
 	UTOOL_CMD_QUERY_PRBS_RESULT = 0x00D5,
 
 	/**
+	 * @UTOOL_CMD_QUERY_FIRMWARE_VERSION: Query firmware version
+	 */
+	UTOOL_CMD_QUERY_FIRMWARE_VERSION = 0x00E1,
+	/**
 	 * @UTOOL_CMD_QUERY_PORT_PKT_STATS: Query statistical indicators at the ub and uboe port
 	 */
 	UTOOL_CMD_QUERY_PORT_PKT_STATS = 0x00E2,
