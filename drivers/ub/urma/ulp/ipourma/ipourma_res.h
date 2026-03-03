1bf9df47335c0b7848bc90eb6be69a4a76b6c074
heyu_1014 <2507175370@qq.com>
2026-03-03 17:11:11 +0800

    urma: introduce ipourma main module
    ANBZ: #IJSBBS
    
    commit 17f7aed7d6f816a280d9b948a7f385ee7c073c97 openEuler
    
    This patch adds the main entry and defination of standard
    types for the ipourma module,provides the creation and
    initialization process for network devices based on ub device.
    
    Signed-off-by: heyu_1014 <2507175370@qq.com>
    Signed-off-by: qiushengming <qiushengming1@huawei.com>

---
+++ drivers/ub/urma/ulp/ipourma/ipourma_res.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021-2025. All rights reserved.
+ *
+ * Description: Functions definition of ipourma_res
+ */
+
+#ifndef _IPOURMA_RES_H
+#define _IPOURMA_RES_H
+
+#include "ipourma_types.h"
+
+int ipourma_reset_rings(struct ipourma_dev_priv *priv);
+
+#endif
