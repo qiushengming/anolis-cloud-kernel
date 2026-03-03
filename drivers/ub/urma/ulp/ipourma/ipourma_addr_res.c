ab0ba33475e9e7f1b91fc6434aa4edbbd554d651
heyu_1014 <2507175370@qq.com>
2026-03-03 17:41:31 +0800

    urma: implement ipourma network device operations
    ANBZ: #IJSBBS
    
    commit 3e7a613d5698496ad5d1845e25d59c88860a2c8b openEuler
    
    This patch implements ipourma's netdev_ops,ethtool_ops and
    netlink_ops, providing the capability to read and configure
    fundamental network device properties, as well as enabling
    data plane communication.
    
    Signed-off-by: heyu_1014 <2507175370@qq.com>
    Signed-off-by: qiushengming <qiushengming1@huawei.com>

--- drivers/ub/urma/ulp/ipourma/ipourma_addr_res.c
+++ drivers/ub/urma/ulp/ipourma/ipourma_addr_res.c
@@ -10,6 +10,31 @@
 #include "ipourma_err.h"
 #include "ipourma_addr_res.h"
 
+STATIC void ipourma_resolve_eids_from_ipv6(struct net_device *dev,
+	struct in6_addr *src, struct in6_addr *dst,
+	union ubcore_eid *src_eid, union ubcore_eid *dst_eid)
+{
+	memcpy(src_eid, src, UBCORE_EID_SIZE);
+	memcpy(dst_eid, dst, UBCORE_EID_SIZE);
+}
+
+void ipourma_resolve_eids(struct net_device *dev, struct sk_buff *skb, u16 proto,
+			  union ubcore_eid *src_eid, union ubcore_eid *dst_eid)
+{
+	struct ipv6hdr *ipv6h;
+
+	switch (proto) {
+	case ETH_P_IPV6:
+		ipv6h = (struct ipv6hdr *)skb_network_header(skb);
+		ipourma_resolve_eids_from_ipv6(dev, &ipv6h->saddr,
+						&ipv6h->daddr, src_eid, dst_eid);
+		break;
+	default:
+		netdev_dbg(dev, "%s\n", ipourma_err_desc(IPOURMA_UNSUPPORTED_ETH_PROTO));
+		break;
+	}
+}
+
 STATIC int ipourma_resolve_ipv6_from_eids(struct net_device *dev,
 	union ubcore_eid *eid, struct in6_addr *addr)
 {
@@ -17,8 +42,8 @@ STATIC int ipourma_resolve_ipv6_from_eids(struct net_device *dev,
 	return IPOURMA_OK;
 }
 
-int ipourma_resolve_ipaddr(struct net_device *dev,
-	u16 proto, union ubcore_eid *eid, struct in6_addr *addr)
+int ipourma_resolve_ipaddr(struct net_device *dev, u16 proto,
+			   union ubcore_eid *eid, struct in6_addr *addr)
 {
 	switch (proto) {
 	case ETH_P_IPV6:
