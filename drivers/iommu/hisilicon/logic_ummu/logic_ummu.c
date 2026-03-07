09d8edb8251b2d53d8cc2f31305aa9004c30d2d3
Liming An <anliming1@h-partners.com>
2026-03-07 14:18:46 +0800

    iommu/ummu: Improve the reliability of ummu initialization
    ANBZ: #IJSBBS
    
    commit d436c180a6f9e887c80aca728a828986b6099ff3 openEuler.
    Disable the UMMU when the initialization fails and exits.
    Invalidating the PLB operation is added during initialization.
    
    Fixes: 0db2fc397b9d ("iommu/ummu: Support UMMU device")
    
    Signed-off-by: Jiashun Wang <wangjiashun@huawei.com>
    Signed-off-by: Liming An <anliming1@h-partners.com>
    Signed-off-by: qiushengming <qiushengming1@huawei.com>

--- drivers/iommu/hisilicon/logic_ummu/logic_ummu.c
+++ drivers/iommu/hisilicon/logic_ummu/logic_ummu.c
@@ -732,8 +732,7 @@ static int logic_domain_set_dirty_ops(struct logic_ummu_domain *logic_domain)
 			return -ENOMEM;
 
 		GEN_IOMMU_DIRTY_OPS(agent_domain->dirty_ops, dirty_ops);
-		ret = xa_err(xa_store(&logic_ummu_ops_info,
-				      (uintptr_t)agent_domain->dirty_ops,
+		ret = xa_err(xa_store(&logic_ummu_ops_info, (uintptr_t)agent_domain->dirty_ops,
 				      dirty_ops, GFP_KERNEL));
 		if (ret) {
 			kfree(dirty_ops);
@@ -759,8 +758,7 @@ static int logic_domain_set_perm_ops(struct logic_ummu_domain *logic_domain)
 			return -ENOMEM;
 
 		GEN_IOMMU_PERM_OPS(agent_domain->perm_ops, perm_ops);
-		ret = xa_err(xa_store(&logic_ummu_ops_info,
-				      (uintptr_t)agent_domain->perm_ops,
+		ret = xa_err(xa_store(&logic_ummu_ops_info, (uintptr_t)agent_domain->perm_ops,
 				      perm_ops, GFP_KERNEL));
 		if (ret) {
 			kfree(perm_ops);
@@ -1917,7 +1915,7 @@ static void logic_ummu_device_del_agent(void)
 
 static int update_logic_ummu(struct ummu_device *ummu)
 {
-	int ret;
+	int ret = 0;
 
 	logic_ummu.ummu_cnt++;
 	list_add_tail(&ummu->list, &logic_ummu.dev_list);
