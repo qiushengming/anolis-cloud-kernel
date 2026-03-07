3de429854238510598decf0341ee6a0dc4a26b8b
Liming An <anliming1@h-partners.com>
2026-03-07 14:42:18 +0800

    iommu/ummu: Remove redundant read register operations
    ANBZ: #IJSBBS
    
    commit 87f1d87b40af36bf7c02292fd20f66d421176805 openEuler.
    ummu driver can directly write to ubmem_mmu register without reading it.
    
    Fixes: ba669149e43b ("iommu/ummu: Add UB Memory support")
    
    Signed-off-by: Jingbin Wu <wujingbin2@huawei.com>
    Signed-off-by: Liming An <anliming1@h-partners.com>
    Signed-off-by: qiushengming <qiushengming1@huawei.com>

--- drivers/iommu/hisilicon/ubmem-mmu/ubmem_mmu.c
+++ drivers/iommu/hisilicon/ubmem-mmu/ubmem_mmu.c
@@ -28,7 +28,8 @@ static LIST_HEAD(ubmem_device_list);
 
 static ulong ubm_granule;
 module_param(ubm_granule, ulong, 0444);
-MODULE_PARM_DESC(ubm_granule, "UB memory page table granule; range [0,7], default(0), addr|size must be aligned to 2M * 2^ubm_granule.");
+MODULE_PARM_DESC(ubm_granule,
+	"UB memory page table granule; range [0,7], default(0), addr|size must be aligned to 2M * 2^ubm_granule.");
 
 static unsigned long ummu_get_ubm_granule(void)
 {
@@ -52,7 +53,6 @@ static unsigned long ummu_get_ubm_granule(void)
 #define MEM_BTE_MASK GENMASK(16, 0)
 
 #define UMMU_MEM_INDEX 0xC
-#define MEM_INDEX_RSV_MASK GENMASK(31, 20)
 #define MEM_WR_MASK (1UL << 19)
 #define MEM_TYPE_MASK (1UL << 18)
 #define MEM_VLD_MASK (1UL << 17)
@@ -347,14 +347,10 @@ static void write_ate_entries(struct ubmem_mmu_domain *dom,
 		start_addr = info->paddr;
 		cnt = info->size / granule_size;
 		for (i = 0; i < cnt; i++) {
-			reg = readl_relaxed(mdev->base + UMMU_MEM_START_ADDR) &
-			      ~START_PTE_ADDR_MASK;
-			reg |= (start_addr & PHYS_ADDR_MASK) >> SZ_2M_SHIFT;
+			reg = (start_addr & PHYS_ADDR_MASK) >> SZ_2M_SHIFT;
 			writel_relaxed(reg, mdev->base + UMMU_MEM_START_ADDR);
 
-			reg = readl_relaxed(mdev->base + UMMU_MEM_INDEX) &
-			      MEM_INDEX_RSV_MASK;
-			reg |= MEM_TYPE_MASK | MEM_WR_MASK | MEM_VLD_MASK;
+			reg = MEM_TYPE_MASK | MEM_WR_MASK | MEM_VLD_MASK;
 			reg |= (index++) & MEM_ATE_INDEX_MASK;
 			writel_relaxed(reg, mdev->base + UMMU_MEM_INDEX);
 			start_addr += granule_size;
@@ -367,23 +363,17 @@ static void write_pte_entry(struct ubmem_mmu_domain *dom,
 {
 	u32 reg;
 
-	reg = readl_relaxed(mdev->base + UMMU_MEM_START_ADDR) &
-	      ~START_PTE_ADDR_MASK;
-	reg |= (dom->iova_start & IOVA_MASK) >> SZ_2M_SHIFT;
+	reg = (dom->iova_start & IOVA_MASK) >> SZ_2M_SHIFT;
 	writel_relaxed(reg, mdev->base + UMMU_MEM_START_ADDR);
 
-	reg = readl_relaxed(mdev->base + UMMU_MEM_LEN_GRANU) &
-	      ~(MEM_GRANU_MASK | MEM_LEN_MASK);
-	reg |= FIELD_PREP(MEM_GRANU_MASK, dom->granule);
+	reg = FIELD_PREP(MEM_GRANU_MASK, dom->granule);
 	reg |= FIELD_PREP(MEM_LEN_MASK, dom->ate_count - 1);
 	writel_relaxed(reg, mdev->base + UMMU_MEM_LEN_GRANU);
 
-	reg = readl_relaxed(mdev->base + UMMU_MEM_BTE) & ~MEM_BTE_MASK;
-	reg |= FIELD_PREP(MEM_BTE_MASK, dom->ate_index_start);
+	reg = FIELD_PREP(MEM_BTE_MASK, dom->ate_index_start);
 	writel_relaxed(reg, mdev->base + UMMU_MEM_BTE);
 
-	reg = readl_relaxed(mdev->base + UMMU_MEM_INDEX) & MEM_INDEX_RSV_MASK;
-	reg |= MEM_WR_MASK | MEM_VLD_MASK;
+	reg = MEM_WR_MASK | MEM_VLD_MASK;
 	reg |= FIELD_PREP(MEM_PTE_INDEX_MASK, dom->base_domain.tid);
 	writel_relaxed(reg, mdev->base + UMMU_MEM_INDEX);
 }
@@ -391,20 +381,16 @@ static void write_pte_entry(struct ubmem_mmu_domain *dom,
 static void clear_pte_entry(struct ubmem_mmu_domain *dom,
 			    struct ubmem_mmu_device *mdev)
 {
-	u32 reg;
+	u32 reg = MEM_WR_MASK;
 
-	reg = readl_relaxed(mdev->base + UMMU_MEM_INDEX) & MEM_INDEX_RSV_MASK;
-	reg |= MEM_WR_MASK;
 	reg |= FIELD_PREP(MEM_PTE_INDEX_MASK, dom->base_domain.tid);
 	writel_relaxed(reg, mdev->base + UMMU_MEM_INDEX);
 }
 
 static void ubmem_mmu_flush_dtlb(struct ubmem_mmu_device *mdev)
 {
-	u32 reg;
+	u32 reg = MEM_DTLB_INVLD_MASK;
 
-	reg = readl_relaxed(mdev->base + UMMU_MEM_DTLB_INVLD) |
-	      MEM_DTLB_INVLD_MASK;
 	writel_relaxed(reg, mdev->base + UMMU_MEM_DTLB_INVLD);
 }
 
