e3dcacabb9ad78dce3c1eef889d76d9e59000592
Liming An <anliming1@h-partners.com>
2026-03-10 19:30:02 +0800

    iommu/ummu: Optimize the definition of 'hisi_ummu_tdev_info'
    ANBZ: #IJSBBS
    
    commit f24a29be087a040ad1fb0dbdd046f13166c5d578 openEuler.
    The hisi_ummu_tdev_info data structure is used externally,
    and its definition is moved from the driver to the 'hisi_ummu.h'
    header file.
    
    Fixes: 7876e979bbdb ("iommu/ummu: Implement domain and core ops in logic UMMU framework")
    
    Signed-off-by: Jiashun Wang <wangjiashun@huawei.com>
    Signed-off-by: Liming An <anliming1@h-partners.com>
    Signed-off-by: qiushengming <qiushengming1@huawei.com>

--- drivers/iommu/hisilicon/ubmem-mmu/ubmem_mmu.c
+++ drivers/iommu/hisilicon/ubmem-mmu/ubmem_mmu.c
@@ -18,8 +18,8 @@
 #include <linux/interrupt.h>
 #include <linux/msi.h>
 #include <ub/ubfi/ubfi.h>
+#include <linux/hisi_ummu.h>
 
-#include "../ummu_cfg_v1.h"
 #include "ubmem_mmu.h"
 
 #define UMMU_MEM_MAX_GRANULE 8
