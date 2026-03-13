ce9580e0a6e0f251ade60ffef14eedd355325664
lairuilang <lawrenlang1@gmail.com>
2026-03-13 18:49:44 +0800

    urma: update urma_admin agg expose
    ANBZ: #IJSBBS
    
    commit 8ee173ec30bd151773b6863785d0c27ce9f5966c openEuler
    
    Read bonding information in ubcore
    
    Signed-off-by: lairuilang <lawrenlang1@gmail.com>
    Signed-off-by: duelu <luyizhou1@huawei.com>
    Signed-off-by: qiushengming <qiushengming1@huawei.com>

--- drivers/ub/urma/ubcore/ubcore_genl.c
+++ drivers/ub/urma/ubcore/ubcore_genl.c
@@ -122,6 +122,13 @@ static const struct genl_ops ubcore_genl_ops[] = {
 		.flags = GENL_ADMIN_PERM,
 		.doit = ubcore_set_sl
 	},
+	{
+		.cmd = UBCORE_CMD_GET_TOPO_BONDING_DEV,
+		.policy = ubcore_policy,
+		.maxattr = ARRAY_SIZE(ubcore_policy) - 1,
+		.flags = GENL_ADMIN_PERM,
+		.doit = ubcore_get_topo_bonding_dev_ops
+	},
 };
 
 /* ubcore family definition */
