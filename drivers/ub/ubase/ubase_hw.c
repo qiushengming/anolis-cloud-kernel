8ea8c56e9774064b51e0fd94947f26ab45c173ff
Xiongchuan Zhou <zhouxiongchuan@h-partners.com>
2026-03-16 20:19:55 +0800

    ub: ubase: The debugfs function supports querying the Ethernet port rate.
    commit 6048ceec44a531dd722a41c9d46fe44737889f0c openEuler.
    
    This patch implements the following functions:
    1. The Ethernet performance statistics function is added.
    The ubase_dbg_dump_perf_stats_eth function is added to the debugfs
    module to collect and display the performance statistics of Ethernet
    ports. The bandwidth usage of a port, including the transmit
    bandwidth and receive bandwidth, can be calculated based on two
    sampling times. The statistics are displayed in kbit/s and contain
    the sampling period information.
    2. The __ubase_perf_stats function is added with the verification
    condition for data_size, and the number of logical ports is
    determined to prevent data overflow caused by insufficient data_size.
    
    Fixes: 9c1792c8e559 ("ub: ubase: adds the function to get ub port stats for unic and other aux driver")
    Signed-off-by: Xiongchuan Zhou <zhouxiongchuan@h-partners.com>
    Signed-off-by: huwentao <huwentao19@h-partners.com>

--- drivers/ub/ubase/ubase_hw.c
+++ drivers/ub/ubase/ubase_hw.c
@@ -979,27 +979,28 @@ static int ubase_stop_perf_stats(struct ubase_dev *udev,
 int __ubase_perf_stats(struct ubase_dev *udev, u64 port_bitmap, u32 period,
 		       struct ubase_perf_stats_result *data, u32 data_size)
 {
-#define UBASE_MS_TO_US(ms) (1000 * (ms))
+#define UBASE_MS_TO_US(ms)	(1000 * (ms))
+
+	unsigned long logic_port_bitmap = udev->caps.dev_caps.logic_port_bitmap;
 	struct ubase_stop_perf_stats_cmd resp;
-	unsigned long logic_port_bitmap;
-	int ret, j, k, port_num;
+	u32 j, k, port_num;
+	int ret;
 	u8 i;
 
 	if (!test_bit(UBASE_STATE_INITED_B, &udev->state_bits) ||
 	    test_bit(UBASE_STATE_RST_HANDLING_B, &udev->state_bits))
 		return -EBUSY;
 
-	logic_port_bitmap = udev->caps.dev_caps.logic_port_bitmap;
-
 	if (port_bitmap) {
 		if (data_size < bitmap_weight((unsigned long *)&port_bitmap,
 					      UBASE_MAX_PORT_NUM) ||
 		    !bitmap_subset((unsigned long *)&port_bitmap,
-				   &logic_port_bitmap,
-				   UBASE_MAX_PORT_NUM))
+				   &logic_port_bitmap, UBASE_MAX_PORT_NUM))
 			return -EINVAL;
 	} else {
-		if (data_size != UBASE_MAX_PORT_NUM)
+		if (data_size != UBASE_MAX_PORT_NUM ||
+		    data_size < bitmap_weight((unsigned long *)&logic_port_bitmap,
+					      UBASE_MAX_PORT_NUM))
 			return -EINVAL;
 
 		port_bitmap = logic_port_bitmap;
