737b131bbed36e8dbbd80359012635072168ac85
Yanlong Zhu <zhuyanlong3@huawei.com>
2026-06-10 11:47:56 +0800

    iommu/ummu: Fix TLB invalidation flow in map/unmap
    ANBZ: #IJSBBS
    
    commit f2e9dddcd089857b771fdc77f6f416e14b1d287e openEuler.
    
    Add domain field to struct ummu_domain which stores iommu_domain
    (IOMMU framework).
    
    Propagate iommu_domain(IOMMU framework) to each struct ummu_domain
    via logic_ummu_probe_finalize(), using sync_iommu_domain helper callback.
    
    Update ummu_tlbi_page() use iommu_domain(IOMMU framework),
    so that iommu_iotlb_gather_add_page() operates on the correct domain.
    
    Refactor ummu_tlbi_walk() to invoke ummu_core_tlb_inv_walk(), which
    in turn calls logic_ummu_tlb_inv_walk() to flush the WALK CACHE on
    each ummu.
    
    Fixes: 473c5dcfba43 ("iommu/ummu: Implement iommu_ops and iommu_domain_ops for UMMU driver")
    
    Signed-off-by: Jiashun Wang<wangjiashun@huawei.com>
    Signed-off-by: Yanlong Zhu <zhuyanlong3@huawei.com>
    Signed-off-by: Shengming Qiu <846759657@qq.com>

--- include/linux/ummu_core.h
+++ include/linux/ummu_core.h
@@ -307,6 +307,9 @@ struct ummu_invalid_cfg_param {
  * @cfg_syn: synchronize configuration table by tid.
  * @tdev_support_attr: Check whether the UMMU device supports the tdev attribute.
  * @get_hw_cap: Get UMMU capability for device.
+ * @tlb_inv_walk: Synchronously invalidate all intermediate TLB state
+ *                (sometimes referred to as the "walk cache") for a virtual
+ *                address range.
  */
 struct ummu_core_ops {
 	int (*get_resource)(struct ummu_base_domain *d, struct resource_args *arg);
@@ -322,7 +325,9 @@ struct ummu_core_ops {
 	int (*get_hw_cap)(struct device *dev, u32 *hw_cap);
 
 	CK_KABI_RESERVE(1)
-	CK_KABI_RESERVE(2)
+	CK_KABI_USE(2, void (*tlb_inv_walk)(struct iommu_domain *domain,
+					    unsigned long iova, size_t size,
+					    size_t granule))
 	CK_KABI_RESERVE(3)
 	CK_KABI_RESERVE(4)
 	CK_KABI_RESERVE(5)
@@ -602,6 +607,17 @@ int ummu_core_fill_pages(struct iova_slot *slot, dma_addr_t iova,
  */
 int ummu_core_drain_pages(struct iova_slot *slot, dma_addr_t iova, unsigned long nr_pages);
 
+/**
+ * ummu_core_tlb_inv_walk() - Synchronously invalidate all intermediate TLB state
+ * (sometimes referred to as the "walk cache") for a virtual address range.
+ * @domain: iommu domain
+ * @iova: IOVA representing the start of the range to be flushed
+ * @size: IOVA representing the end of the range to be flushed (inclusive)
+ * @granule: The interval at which to perform the flush
+ */
+void ummu_core_tlb_inv_walk(struct iommu_domain *domain, unsigned long iova,
+			    size_t size, size_t granule);
+
 #else
 static inline int ummu_dev_enable_feat(struct device *dev, enum iommu_dev_features f)
 {
@@ -651,6 +667,12 @@ static inline int ummu_core_drain_pages(struct iova_slot *slot, dma_addr_t iova,
 {
 	return -EOPNOTSUPP;
 }
+
+static inline void ummu_core_tlb_inv_walk(struct iommu_domain *domain, unsigned long iova,
+					  size_t size, size_t granule)
+{
+}
+
 #endif /* CONFIG_UB_UMMU_CORE */
 
 #if IS_ENABLED(CONFIG_UB_UMMU_CORE_DRIVER)
